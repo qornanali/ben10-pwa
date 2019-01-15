6086268e57573cadfcea5956516036d7ee56465d
Ali <aliqornan@ymail.com>
2019-01-15 19:13:05 +0700

    Add js to loading page files from pages/ directory

--- js/nav.js
+++ js/nav.js
@@ -17,4 +17,26 @@ document.addEventListener("DOMContentLoaded", function() {
     xhttp.open("GET", "nav-menus.html", true);
     xhttp.send();
   }
+
+  var page = window.location.hash.substr(1);
+  if (page == "") page = "home";
+  loadPage(page);
+  
+  function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        var content = document.querySelector("#body-content");
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML = "<p>Page not found.</p>";
+        } else {
+          content.innerHTML = "<p>Ups.. page cannot be accessed.</p>";
+        }
+      }
+    };
+    xhttp.open("GET", "pages/" + page + ".html", true);
+    xhttp.send();
+  }
 });
